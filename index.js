34835ddf42cb69d12bfe307eaf08ce0ee9dd4c88
azzmnrwebdev <Azzamnralwmansyur7273@gmail.com>
2022-03-14 04:38:27 +0700

    tugas javascript es6

--- index.js
+++ index.js
@@ -1,87 +1,96 @@
-/**
- * Soal 1
- * ini jawaban soal nomor 1
- */
-console.log("Jawaban dari soal nomer 1");
-
-var daftarHewan = ["2. Komodo", "5. Buaya", "3. Cicak", "4. Ular", "1. Tokek"];
-
-daftarHewan.sort();
-
-daftarHewan.forEach(function (item) {
-  console.log(item);
-});
-
-console.log(" ");
+console.log("---------- Awal Mulai Mengerjakan ----------");
 
 /**
- * Soal 2
- * ini jawaban soal nomor 2
+ * Soal nomor 1
+ * buatlah fungsi menggunakan arrow function luas dan keliling persegi panjang
+ * dengan arrow function lalu gunakan let atau const di dalam soal ini.
  */
-console.log("Jawaban dari soal nomer 2");
-
-function introduce(data) {
-  return `Nama saya ${data.name}, umur saya ${data.age} tahun, alamat saya di ${data.address}, dan saya punya hobby yaitu ${data.hobby}!`;
-}
-
-var data = {
-  name: "John",
-  age: 30,
-  address: "Jalan Pelesiran",
-  hobby: "Gaming",
+console.log("Jawaban Nomor 1");
+const hitung = (panjang, lebar) => {
+  console.log("Hasil luas persegi panjang adalah : " + panjang * lebar);
+  console.log(
+    "Hasil keliling persegi panjang adalah: " + 2 * (panjang + lebar)
+  );
 };
 
-var perkenalan = introduce(data);
-console.log(perkenalan);
+hitung(10, 5);
 
-console.log(" ");
-
-/**
- * Soal 3
- * ini jawaban soal nomor 3
- */
-console.log("Jawaban dari soal nomer 3");
-
-// membuat function hitung_huruf_vokal
-function hitung_huruf_vokal(str) {
-  // menemukan jumlah vokal
-  const count = str.match(/[aeiou]/gi).length;
-
-  // mengembalikan jumlah vokal
-  return count;
-}
-
-var hitung_1 = hitung_huruf_vokal("Muhammad");
-
-var hitung_2 = hitung_huruf_vokal("Iqbal");
-
-console.log(hitung_1, hitung_2); // 3 2
-
-console.log(" ");
+console.log("");
 
 /**
- * Soal 4
- * ini jawaban soal nomor 4
+ * Soal nomor 2
+ * Ubahlah code di bawah ini ke dalam arrow function
+ * dan object literal es6 yang lebih sederhana
  */
-console.log("Jawaban dari soal nomer 4");
+console.log("Jawaban Nomor 2");
+const newFunction = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName,
+    fullName() {
+      console.log(firstName + " " + lastName);
+    },
+  };
+};
 
-function hitung(numbers) {
-  return 2 + (numbers - 2) * 2;
-  /**
-   * cara bacanya seperti ini :
-   * hitung yang ada di dalam kurung terlebih dahulu
-   * baru dikali 2 dan ditambahkan 2
-   *
-   * contoh :
-   * parameter 5 - 2 = 3,
-   * 3 dikali 2 sama dengan 6,
-   * 6 ditambah 2 menjadi 8
-   * jadi hasil nya adalah '8'
-   */
-}
+newFunction("William", "Imoh").fullName();
 
-console.log(hitung(0)); // -2
-console.log(hitung(1)); // 0
-console.log(hitung(2)); // 2
-console.log(hitung(3)); // 4
-console.log(hitung(5)); // 8
+console.log("");
+
+/**
+ * Soal nomor 3
+ * Diberikan sebuah object sebagai berikut:
+ *  const newObject = {
+ *      firstName: "Muhammad",
+ *      lastName: "Iqbal Mubarok",
+ *      address: "Jalan Ranamanyar",
+ *      hobby: "playing football",
+ *  }
+ *
+ * dalam ES5 untuk mendapatkan semua nilai dari object tersebut kita harus tampung satu per satu:
+ *  const firstName = newObject.firstName;
+ *  const lastName = newObject.lastName;
+ *  const address = newObject.address;
+ *  const hobby = newObject.hobby;
+ *
+ * Gunakan metode destructuring dalam ES6 untuk mendapatkan semua nilai dalam object dengan lebih singkat (1 line saja)
+ */
+console.log("Jawaban Nomor 3");
+const person = {
+  firstName: "Muhammad",
+  lastName: "Iqbal Mubarok",
+  address: "Jalan Ranamanyar",
+  hobby: "Playing Football",
+};
+const { firstName, lastName, address, hobby } = person;
+console.log(firstName, lastName, address, hobby);
+
+console.log("");
+
+/**
+ * Soal nomor 4
+ * Kombinasikan dua array berikut menggunakan array spreading ES6
+ */
+console.log("Jawaban Nomor 4");
+
+const west = ["Will", "Chris", "Sam", "Holly"];
+const east = ["Gill", "Brian", "Noel", "Maggie"];
+const combined = [...west, ...east];
+
+console.log(combined);
+
+console.log("");
+
+/**
+ * Soal nomor 5
+ * sederhanakan string berikut agar menjadi lebih
+ * sederhana menggunakan template literals ES6:
+ */
+console.log("Jawaban Nomor 5");
+const planet = "earth";
+const view = "glass";
+console.log(
+  `Lorem ${view} dolor sit amet, consectetur adipiscing alit, ${planet}`
+);
+
+console.log("---------- Selesai Sudah Mengerjakan ----------");
