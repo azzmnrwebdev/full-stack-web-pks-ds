67356a127f992c1dd788afa06e0c977cfc66b504
azzmnrwebdev <Azzamnralwmansyur7273@gmail.com>
2022-03-14 01:18:55 +0700

    memasangkan template dengan laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+</head>
+
+<body>
+    <h1 style="color: #7e0de7;">Media Online</h1>
+    <h3 style="color: #18d48c;">Sosial Media Developer</h3>
+    <p>Belajar dan berbagi agar hidup menjadi lebih baik</p>
+    <h3 style="color: #d87093;">Benefit Join di Media Online</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama para Developer</li>
+        <li>Sharing knowlenge</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h3 style="color: #14c0cc;">Cara Bergabung ke Media Online</h3>
+    <ol>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftarkan di <a href="/register" style="text-decoration: none;color: blue;"
+                title="SIlahkan Register"><b>Form Sign
+                    Up</b></a></li>
+        <li>Selesai</li>
+    </ol>
+</body>
+
+</html>
